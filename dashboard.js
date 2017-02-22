7b33eaf642978ab4a3c4535900d5728b9ec0aa77
Vikhyath <vikhyatc@thoughtworks.com>
2017-02-22 18:28:17 +0530

    Renamed popup to dashboard and Updated icon <Viky>

--- dashboard.js
+++ dashboard.js
@@ -14,7 +14,7 @@ loadCssFile = function() {
   var fileref = document.createElement("link");
   fileref.setAttribute("rel", "stylesheet");
   fileref.setAttribute("type", "text/css");
-  fileref.setAttribute("href", "popup.css");
+  fileref.setAttribute("href", "dashboard.css");
   if (typeof fileref != "undefined")   document.getElementsByTagName("head")[0].appendChild(fileref)
 }
 
